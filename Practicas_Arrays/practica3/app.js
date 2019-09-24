edef18ad9ccfc6b97584baf77fb378fdcef86658
luis solis <00269648luis@gmail.com>
2019-09-24 12:13:51 -0500

    practica 3 modificada a que agregue numeros RANDOM y no solicitados al usuario

--- Practicas_Arrays/practica3/app.js
+++ Practicas_Arrays/practica3/app.js
@@ -1,17 +1,13 @@
 let btnCalcular=document.getElementById('btnCalcular');
 let limite=document.getElementById('limite');
-let numero=document.getElementById('numero');
 let imprimir=document.getElementById('imprimir');
 let imprimir2=document.getElementById('imprimir2');
 let arrayNumeros=[];
 let arrayPares=[];
-let tam=true;
 let contLetras=0;
 let resultado="";
 let resultado2="";
 let lim=0;
-let x=0;
-let y=0;
 
 
 eventListener();
@@ -21,31 +17,23 @@ function eventListener(){
 }
 
 function calcular(){
-    let num=numero.value;
+    resultado="";
+    resultado2="";
+    let num=0;
     let lim=Number(limite.value);
-
-    if(tam==true){
-    for(x; x<lim; x++){
+    for(let x=0; x<lim; x++){
+        num=Math.floor((Math.random() * 100) + 1);
         arrayNumeros[x]=num;        
-        resultado+=`${arrayNumeros[x]} <br>`;
+        resultado+=`[${x}]=${arrayNumeros[x]} <br>`;
         imprimir.innerHTML=resultado;
         if(arrayNumeros[x]%2==0){ 
             arrayPares[x]=arrayNumeros[x];
-            resultado2+=`${arrayPares[x]} <br>`;
+            resultado2+=`[${x}]=${arrayPares[x]} <br>`;
             imprimir2.innerHTML=resultado2;
-        }  
-        x++;
-        return; 
-        
-    }
-
-}if(x==lim){   
-    Swal.fire({
-        type: 'error',
-        title: 'Exedio el numero de palabras...',
-        text: 'favor de aumentar el numero de palabras a evaluar',
-      })
+        }         
 }
+
+
    
 
 
